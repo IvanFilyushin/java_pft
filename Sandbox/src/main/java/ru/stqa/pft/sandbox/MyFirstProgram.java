589a47275687044a1d2f836bdb9541c925b297d5
IvanFilyushin <ivan.filyushin@gmail.com>
2016-07-15 22:44:52 +0300

    Реализация метода distance

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,21 +5,11 @@ class MyFirstProgram {
 	public static void main(String args[])
 	{
 
-      Point p1=new Point();
-      p1.x=1;
-      p1.y=1;
-      Point p2=new Point();
-      p2.x=5;
-      p2.y=5;
+      Point p1=new Point(1,1);
+      Point p2=new Point(5,5);
 
-    System.out.println(distance(p1,p2));
+
+    System.out.println(p1.distance(p2));
 	}
-
-  public static double distance(Point p1, Point p2) {
-
-    double l;
-    l= Math.sqrt ((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
-    return l;
-  }
 
 }
