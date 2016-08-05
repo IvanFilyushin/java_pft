9aa30652ed246c591f1fd8273ff196e6f30abf36
IvanFilyushin <ivan.filyushin@gmail.com>
2016-08-05 16:55:49 +0300

    Реализация теста после модификации контакта с применением сравнения множеств

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -24,13 +25,17 @@ public class ContactModificationTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().gotoModificationContactPage();
-    app.getContactHelper().fillContactForm(new ContactData("rename", "name2", "name3", "title", "company",
-            "address", "phone1", "phone2", "phone3",
-            null), false);
+    ContactData contact = new ContactData("rename", "name2", "name3", "title", "company",
+            "address", "phone1", "phone2", "phone3",null);
+    app.getContactHelper().fillContactForm(contact,false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size());
+
+    before.remove(before.size()-1);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 
