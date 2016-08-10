93255e144acb1cc89476ebf20b55259262ecaa68
IvanFilyushin <ivan.filyushin@gmail.com>
2016-08-10 13:52:02 +0300

    Реализация теста создания группы сравнением упорядоченных списков
    Упорядочены по  Id, сравнены по имени и фамилии

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -46,7 +46,6 @@ public class ContactData {
 
     ContactData that = (ContactData) o;
 
-    if (id != that.id) return false;
     if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
     return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
 
@@ -54,8 +53,7 @@ public class ContactData {
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    int result = firstname != null ? firstname.hashCode() : 0;
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
     return result;
   }
@@ -63,7 +61,7 @@ public class ContactData {
   public ContactData(String firstname, String lastname, String nickname,
                      String title, String company, String address, String phone1, String phone2, String phone3,
                      String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.nickname = nickname;
@@ -71,6 +69,7 @@ public class ContactData {
     this.title = title;
     this.company = company;
     this.address = address;
+
     this.phone1 = phone1;
     this.phone2 = phone2;
     this.phone3 = phone3;
