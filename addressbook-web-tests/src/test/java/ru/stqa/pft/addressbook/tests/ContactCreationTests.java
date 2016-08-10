3244627cd57e5c6ff2c1993ea4cff7492d2080eb
IvanFilyushin <ivan.filyushin@gmail.com>
2016-08-10 12:38:29 +0300

    Реализация теста добавления контакта проверкой множеств
    цикл for для выбора максимального идентификатора

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -14,11 +15,25 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
 
     app.getContactHelper().gotoAddContactPage();
-    app.getContactHelper().createContact(new ContactData("name1", "name2", "name3",
+    ContactData contact = new ContactData("name1", "name2", "name3",
             "title", "company", "address", "phone1", "phone2", "phone3",
-            "test1"));
+            "test1");
+    app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
-    Assert.assertEquals(after.size(),before.size()+1);
-  }
+    Assert.assertEquals(after.size(), before.size() + 1);
 
+    int max = 0;
+    for (ContactData c : after) {
+      if (c.getId() > max) {
+        max = c.getId();
+      }
+    }
+    contact.setId(max);
+    before.add(contact);
+
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+  }
 }
+
+
