4ad4bcfa2498e10e2d0561dc955ceb6f3412c1ac
IvanFilyushin <ivan.filyushin@gmail.com>
2016-08-19 15:45:06 +0300

    Реализация параметризованных тестов создания группы и контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook.generators;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ольга on 19.08.2016.
+ */
+public class ContactDataGenerator {
+
+  public static void main(String[] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts,file);
+
+  }
+
+  private static void save(List<ContactData> contacts, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for(ContactData contact:contacts){
+      writer.write(String.format("%s;%s;%s;%s;%s;\n", contact.getFirstname(),contact.getLastname(),contact.getAddress(),
+      contact.getEmail1(),contact.getPhone1()));
+    }
+    writer.close();
+  }
+
+  private static List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for(int i = 0;i < count; i++){
+      contacts.add(new ContactData().withFirstName("name"+i).withLastName("lastname"+i).withAddress("address"+i)
+              .withEmail1("email"+i).withPhone1("phone"+i));
+    }
+    return contacts;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,46 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts(){
+
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstName("fname 1").withLastName("lname 1").withNickName("nname 1")
+            .withTitle("title1").withCompany("company1")
+            .withAddress("address1").withPhone1("Phone 1").withPhone2("Phone 2").withPhone3("Phone 3")
+            .withEmail1("Email1").withEmail2("Email2").withGroup("test1")
+            .withPhoto(new File("src/test/resources/zt.png"))});
+    list.add(new Object[] {new ContactData().withFirstName("fname 2").withLastName("lname 2").withNickName("nname 2")
+            .withTitle("title2").withCompany("company2")
+            .withAddress("address2").withPhone1("Phone1 2").withPhone2("Phone2 2").withPhone3("Phone3 2")
+            .withEmail1("Email1 2").withEmail2("Email2 2").withGroup("test1")
+            .withPhoto(new File("src/test/resources/zt.png"))});
+
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
 
     Contacts before = app.contact().all();
 
     app.contact().gotoAddPage();
-    File photo = new File("src/test/resources/zt.png");
-    ContactData contact = new ContactData().withFirstName("name1")
-            .withLastName("name2").withNickName("name3").withTitle("title").withCompany("company")
-            .withAddress("address").withPhone1("8(903)125-14-62").withPhone2("745-85-244").withPhone3("100 00 52")
-            .withEmail1("as@co.com").withEmail2("petrus@zw.ru").withGroup("test1").withPhoto(photo);
+//   File photo = new File("src/test/resources/zt.png");
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size()+1));
     Contacts after = app.contact().all();
