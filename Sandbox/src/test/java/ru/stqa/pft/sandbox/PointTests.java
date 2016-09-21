bed0bc2dca011cf31b31b92ba70e1e252993d619
Ivan Filyushin <ivan.filyushin@gmail.com>
2016-09-21 11:49:26 +0400

    Изменение проверочного значения

--- Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -12,7 +12,7 @@ public class PointTests {
   public void testArea(){
     Point p1=new Point(5,5);
     Point p2=new Point(0,0);
-    Assert.assertEquals(p1.distance(p2),7.071067811865475);
+    Assert.assertEquals(p1.distance(p2),7.0710678118654755);
   }
 
 }
